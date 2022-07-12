c80c535613139572947c9502e71800cfcfeedad2
abhay-h <abhaytara18@gmail.com>
2022-07-12 23:49:52 +0530

    l

---
+++ main.js
@@ -0,0 +1,12 @@
+const title = document.querySelector('#main');
+const listItems = document.querySelectorAll('.list-item');
+
+/* // title.style.color = 'red';
+title.style.fontSize = '100px'; */
+
+ 
+
+for (let i = 0; i < listItems.length; i++) {
+   
+    listItems[i].style.fontSize = '500px';
+}
